ce6def9ab413fddc29c7039d0ae330220b3012a5
Xixuan.chen <xixuan.chen@cloudminds.com>
2021-03-15 10:10:28 +0800

    Xixuan.chen

---
+++ twotest/js/index.js
@@ -0,0 +1,172 @@
+(function () {
+  var mychart = echarts.init(document.querySelector('.bar'));
+  var option = {
+    color: ["#2f89cf"],
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        // 坐标轴指示器，坐标轴触发有效
+        type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+      }
+    },
+    grid: {
+      left: "0%",
+      top: "10px",
+      right: "0%",
+      bottom: "4%",
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: "category",
+        data: [
+          "旅游行业",
+          "教育培训",
+          "游戏行业",
+          "医疗行业",
+          "电商行业",
+          "社交行业",
+          "金融行业"
+        ],
+        axisTick: {
+          alignWithLabel: true
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: "12"
+          }
+        },
+        axisLine: {
+          show: false
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: "12"
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+            // width: 1,
+            // type: "solid"
+          }
+        },
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: "直接访问",
+        type: "bar",
+        barWidth: "35%",
+        data: [200, 300, 300, 900, 1500, 1200, 600],
+        itemStyle: {
+          barBorderRadius: 5
+        }
+      }
+    ]
+  };
+  mychart.setOption(option);
+  window.addEventListener('resize', function () {
+    mychart.resize()
+  })
+})();
+
+(function () {
+  var mychart = echarts.init(document.querySelector('.bar2'));
+  var data = [70, 34, 60, 78, 69];
+  var titlename = ["HTML5", "CSS3", "javascript", "VUE", "NODE"];
+  var valdata = [702, 350, 610, 793, 664];
+  var myColor = ["#1089E7", "#F57474", "#56D0E3", "#F8B448", "#8B78F6"];
+  var option = {
+    grid: {
+      top: "10%",
+      left: "22%",
+      bottom: "10%"
+    },
+    xAxis: {
+      show: false
+    },
+    yAxis: [
+      {
+        type: 'category',
+        data: titlename,
+        inverse: true,
+        axisLine: {
+          show: false
+        },
+        splitLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          color: "#fff",
+        }
+      },
+      {
+        show: true,
+        inverse: true,
+        data: valdata,
+        axisLabel: {
+          textStyle: {
+            fontSize: 12,
+            color: "#fff"
+          }
+        }
+      }],
+    series: [
+      {
+        name: '条',
+        type: 'bar',
+        data: data,
+        itemStyle: {
+          barBorderRadius: 20
+        },
+        barCategoryGap: 50,
+        barWidth: 10,
+        itemStyle: {
+          barBorderRadius: 20,
+          color: function (params) {
+            return myColor[params.dataIndex];
+          }
+        },
+        label: {
+          show: true,
+          position: "inside",
+          formatter: "{c}%"
+        }
+      },
+      {
+        name: "框",
+        type: "bar",
+        yAxisIndex: 1,
+        barCategoryGap: 50,
+        data: [100, 100, 100, 100, 100],
+        barWidth: 15,
+        itemStyle: {
+          color: "none",
+          borderColor: "#00c1de",
+          borderWidth: 3,
+          barBorderRadius: 15
+        }
+      }
+    ]
+  };
+  mychart.setOption(option);
+  window.addEventListener('resize', function () {
+    mychart.resize();
+  })
+})();
